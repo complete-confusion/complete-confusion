d94f2541c239b3208ef1872c7de4ee3e5c015d98
Christiaan Meijer <c.meijer@esciencecenter.nl>
2025-07-10 14:11:34 +0200

    add image representation to original data (fixes #13)

--- tests/performance_metrics_test_data.py
+++ tests/performance_metrics_test_data.py
@@ -10,9 +10,10 @@ class PerformanceMetricsTestData:
     name: str
     predictions: Collection[int]
     truths: Collection[int]
-    probabilities: Optional[Collection[Collection[float]]]
-    class_list: Optional[Collection[str]]
-    text_representations: Optional[Collection[str]]
+    probabilities: Optional[Collection[Collection[float]]] = None
+    class_list: Optional[Collection[str]] = None
+    text_representations: Optional[Collection[str]] = None
+    image_representations: Optional[Collection[str]] = None
 
     def __str__(self):
         return self.name
@@ -20,7 +21,8 @@ class PerformanceMetricsTestData:
 
 def get_cifar10_test_data() -> PerformanceMetricsTestData:
     """ Read cifar10 data from csv and return TestData object """
-    df = pd.read_csv(Path('tests') / 'test_data' / 'cifar10_test_data.csv')
+    test_data_path = Path('tests') / 'test_data'
+    df = pd.read_csv(test_data_path / 'cifar10_test_data.csv')
 
     # Extract predictions, true labels, probabilities, and class list
     predictions = df["prediction"].tolist()
@@ -33,7 +35,7 @@ def get_cifar10_test_data() -> PerformanceMetricsTestData:
         truths=trues,
         probabilities=None,
         class_list=class_list,
-        text_representations=None,
+        image_representations=[str(Path(test_data_path / "dummy_image.jpg").absolute().as_uri()) for _ in predictions]
     )
 
 
